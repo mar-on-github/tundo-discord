443fb0005cdce640f0c5880c7aed20306f6909bc
Mar <just.mar.ok2813@gmail.com>
2022-09-21 10:24:14 +0200

    Add version naming

--- commands/server.js
+++ commands/server.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder, Guild, Client, userMention, User } = require('discord.js');
+const {version} = require ('../package.json');
 var Invite = "The main invite for my server is https://discord.gg/gBhR2bzzMw";
 if (Client.Guild != '853925881411141652') var Invite = "Heh... I'm not in my home server.";
 if (Client.Guild = '1021885462530494504') var Invite = "This is my development server. Please do not invite friends over here.";
@@ -8,7 +9,7 @@ module.exports = {
 		.setName('server')
 		.setDescription('Display info about this server.'),
 	async execute(interaction) {
-		TheReply = `:name_badge: _Server name_: \`${interaction.guild.name}\`\n:1234: _Total members_: \`${interaction.guild.memberCount}\`\n\n\n:arrow_forward: ${Invite}`
+		TheReply = `:name_badge: _Server name_: \`${interaction.guild.name}\`\n:1234: _Total members_: \`${interaction.guild.memberCount}\`\n:point_up: _Picobot version_: \`${version}\`\n\n\n:arrow_forward: ${Invite}`
 		return interaction.reply(TheReply);
 	},
 };
--- events/ready.js
+++ events/ready.js
@@ -1,12 +1,14 @@
 const { Integration, Message, Client} = require("discord.js");
 const { ChannelIDs } = require('../config.json');
+const { version } = require('../package.json');
 module.exports = {
     name: 'ready',
     once: true,
     
     execute(client) {
-        console.log(`Ready! Logged in as ${client.user.tag}`);
+        tolog = `Ready! Logged in as ${client.user.tag}. Running version ${version}.`;
+        console.log(tolog)
         const channelID = ChannelIDs.picolog;
-        client.channels.cache.get(channelID).send(`Ready! Logged in as ${client.user.tag}`);
+        client.channels.cache.get(channelID).send(tolog);
     },
 };
