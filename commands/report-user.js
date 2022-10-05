e83ddd66af94b976dda394e4b9025f35797205c7
Mar <just.mar.ok2813@gmail.com>
2022-10-05 15:02:38 +0200

    0.1.3 indev jump (#2)
    * made a start. the command user-side is complete, I guess.
    
    * I'm pushing an update for things to speed up a bit
    
    * The jump

--- commands/report-user.js
+++ commands/report-user.js
@@ -4,10 +4,15 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('report')
         .setDescription('Report a user to the mods.')
-        .addUserOption(option => option.setName('target').setDescription('The user\'s avatar to show')),
+        .addUserOption(option => option.setName('user').setDescription('The user you report.').setRequired(true))
+        .addStringOption(option => option.setName('reason').setDescription('What did this user do and when?').setRequired(true))
+        .addAttachmentOption(option => option.setName('screenshot').setDescription('Evidence, or something that shows us there is something wrong.').setRequired(false)),
+        
     async execute(interaction) {
         const user = interaction.options.getUser('target');
-        if (user) return interaction.reply(`${user.username}'s avatar: ${user.displayAvatarURL({ dynamic: true })}`);
-        return interaction.reply(`Your avatar: ${interaction.user.displayAvatarURL()}`);
+        if (user) return(
+            interaction.reply(`${user.username}'s avatar: ${user.displayAvatarURL({ dynamic: true })}`)
+            )
+        interaction.user
     },
 };
--- events/msgtriggers.js
+++ events/msgtriggers.js
@@ -1,22 +1,33 @@
 function makeLowerCase(value) {
     return value.toString().toLowerCase();
 }
+const { Client, GatewayIntentBits} = require('discord.js');
 const { ChannelIDs } = require('../config.json');
-
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessageTyping] });
 module.exports = {
     name: 'messageCreate',
     once: false,
     execute(message) {
         if (makeLowerCase(message).includes('discord.gg')) {
+            if ((message.channel.id) != (ChannelIDs.selfpromo)) {
+                if ((message.channel.id) != (ChannelIDs.partners)) {
             message.reply('Hi there! Looking for a partnership? Contact our staff \(\<@&966969999644319786\>\) please!')
             // console.log(`**Category: Selfpromo**\n\n\n ${message.author.tag} sent \`${message}\`in \<#${message.channel.id}\>`);
+            }}
         }
         if (makeLowerCase(message).includes('/boop')) {
-            if ((message.channel.id) = (ChannelIDs.general))
+            if ((message.channel.id) = (ChannelIDs.general)) {
             message.reply('You can\'t boop me here!')
+            }
         }
         if (makeLowerCase(message).includes('pico')) {
-            message.reply("That's my name!")
+            if ((message.channel.id) != (ChannelIDs.selfpromo)) {
+                if ((message.channel.id) != (ChannelIDs.partners)) {
+                    if (Math.random() < 0.1) {
+                    message.reply("That's my name!")
+                    }
+                }
+            }
         }
         // console.log(`${message.author.tag} sent \`${message}\`in #${message.channel.name}`);
     },
